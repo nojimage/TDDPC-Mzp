829d406e9fc6a3cf34aed381f7d175fc1db62c17
nojimage <nojimage@gmail.com>
2011-05-29 15:32:12 +0900

    fizzBuzz関数の実装

--- libs/fizz_buzz.php
+++ libs/fizz_buzz.php
@@ -4,7 +4,26 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-function fizzBuzz($int){
-    return 'Fizz';
+
+function fizzBuzz($int=null) {
+    if(is_null($int)){
+        return '';
+     }
+    if(is_string($int)){
+        return $int;
+    }
+    if($int ==0){
+        return "0";
+     }
+    if ($int % 15 ==0) {
+        return 'FizzBuzz';
+    }
+    if ($int % 5 == 0) {
+        return 'Buzz';
+    }
+    if ($int % 3 == 0) {
+        return 'Fizz';
+    }
+    return (string) $int;
 }
 ?>
--- tests/cases/libs/fuzz_buzz.test.php
+++ tests/cases/libs/fuzz_buzz.test.php
@@ -11,12 +11,82 @@ class FizzBuzzTestCase extends CakeTestCase {
     public $fixtures = array();
 
     public function startTest($method) {
+        
     }
 
     public function endTest($method) {
+        
     }
 
     public function testFizzBuzz() {
-        $this->assertIdentical('Fizz', fizzBuzz(3));
+        $this->assertIdentical('Fizz', fizzBuzz(3), '3の倍数でFizzを出力する: %s');
+    }
+
+    public function testFizzBuzz5の倍数はBuzz() {
+        $this->assertIdentical('Buzz', fizzBuzz(5), '5の倍数でBuzzを出力する: %s');
+    }
+
+    public function testFizzBuzz3と5の倍数はFizzBuzz() {
+        $this->assertIdentical('FizzBuzz', fizzBuzz(15), '3と5の倍数でFizzBuzzを出力する: %s');
+    }
+
+    public function testFizzBuzz6はFizz() {
+        $this->assertIdentical('Fizz', fizzBuzz(6));
+    }
+
+    public function testFizzBuzz10はBuzz() {
+        $this->assertIdentical('Buzz', fizzBuzz(10));
+    }
+
+    public function testFizzBuzz1は1() {
+        $this->assertIdentical('1', fizzBuzz(1));
+    }
+
+    public function testFizzBuzz30はFizzBuzz() {
+        $this->assertIdentical('FizzBuzz', fizzBuzz(30), '30でFizzBuzzを出力する: %s');
+    }
+
+    public function testFizzBuzz2は2() {
+        $this->assertIdentical('2', fizzBuzz(2));
+    }
+
+    public function testFizzBuzz4は4() {
+        $this->assertIdentical('4', fizzBuzz(4));
+    }
+
+    public function testFizzBuzz7は7() {
+        $this->assertIdentical('7', fizzBuzz(7));
+    }
+
+    public function testFizzBuzz0は0() {
+        $this->assertIdentical('0', fizzBuzz(0));
+    }
+
+    public function testFizzBuzz文字列は文字列で返す() {
+        $this->assertIdentical('ほげほげほげ', fizzBuzz('ほげほげほげ'));
+    }
+
+    public function testFizzBuzz負の3の倍数はFizz() {
+        $this->assertIdentical('Fizz', fizzBuzz(-3));
+    }
+
+    public function testFizzBuzz負の5の倍数はBuzz() {
+        $this->assertIdentical('Buzz', fizzBuzz(-5));
+    }
+
+    public function testFizzBuzz負の３と5の倍数はFizzBuzz() {
+        $this->assertIdentical('FizzBuzz', fizzBuzz(-15));
+    }
+    
+    public function testFizzBuzz数字の入っている文字列は文字列で返す() {
+        $this->assertIdentical('ほげ3ほげほげ', fizzBuzz('ほげ3ほげほげ'));
+    }
+    
+    public function testFizzBuzz空文字列は空文字列で返す() {
+        $this->assertIdentical('', fizzBuzz(''));
+    }
+    
+    public function testFizzBuzzNullは空文字列で返す() {
+        $this->assertIdentical('', fizzBuzz());
     }
 }

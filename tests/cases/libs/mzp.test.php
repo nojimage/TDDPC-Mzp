208e61b426619683ed0148db739f27dd56f58595
nojimage <nojimage@gmail.com>
2011-05-29 18:01:07 +0900

    [from now] 2011年 5月29日 日曜日 18時01分07秒 JST
    diff --git a/tests/cases/libs/mzp.test.php b/tests/cases/libs/mzp.test.php
    index d8b7ffe..dc5906c 100644
    --- a/tests/cases/libs/mzp.test.php
    +++ b/tests/cases/libs/mzp.test.php
    @@ -42,8 +42,7 @@ class MzpTestCase extends CakeTestCase {
         function testGetDataでデータをとってくる() {
             $results = $this->Mzp->getData();
             $this->assertTrue(is_array($results));
    -        #$this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
    -        $this->assertPattern('!.+\t.+\t.*!', $results[0]);
    +        $this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
         }
    
         function testHttpSocketが定義されている() {

--- tests/cases/libs/mzp.test.php
+++ tests/cases/libs/mzp.test.php
@@ -42,8 +42,7 @@ class MzpTestCase extends CakeTestCase {
     function testGetDataでデータをとってくる() {
         $results = $this->Mzp->getData();
         $this->assertTrue(is_array($results));
-        #$this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
-        $this->assertPattern('!.+\t.+\t.*!', $results[0]);
+        $this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
     }
     
     function testHttpSocketが定義されている() {

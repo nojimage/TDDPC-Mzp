7fe2e402825d10fee72ae5d4f0258d3e4cdef70f
nojimage <nojimage@gmail.com>
2011-05-29 17:39:21 +0900

    [from now] 2011年 5月29日 日曜日 17時39分21秒 JST
    diff --git a/tests/cases/libs/mzp.test.php b/tests/cases/libs/mzp.test.php
    index 1258edb..b77cdf4 100644
    --- a/tests/cases/libs/mzp.test.php
    +++ b/tests/cases/libs/mzp.test.php
    @@ -13,22 +13,35 @@ class MzpTestCase extends CakeTestCase {
             ClassRegistry::flush();
         }
    
    -    function testTweetをパースする(){
    +    function testTweetをパースする() {
             $this->assertIdentical("Norlal\tあいうえお", $this->Mzp->categorize("Alice\tあいうえお"));
         }
    -    function testTweetをパースする2(){
    +
    +    function testTweetをパースする2() {
             $this->assertIdentical("Norlal\tかきくけこ", $this->Mzp->categorize("Bob\tかきくけこ"));
         }
    -    function testTweetをパースするrelpyを渡すとReplyをつけて返す(){
    +
    +    function testTweetをパースするrelpyを渡すとReplyをつけて返す() {
             $this->assertIdentical("Reply\t@Bob", $this->Mzp->categorize("AAA\t@Bob"));
         }
    -    function testTweetをパースするmentionを渡すとMentionをつけて返す(){
    +
    +    function testTweetをパースするmentionを渡すとMentionをつけて返す() {
             $this->assertIdentical("Mention\tあああ@Bob", $this->Mzp->categorize("AAA\tあああ@Bob"));
         }
    -    function testTweetをパースするhashtagを渡すとHashTagをつけて返す(){
    +
    +    function testTweetをパースするhashtagを渡すとHashTagをつけて返す() {
             $this->assertIdentical("!HashTag\tあああ #tddpc", $this->Mzp->categorize("AAA\tあああ #tddpc"));
         }
    -    function testTweetをパースするhashtagとreplyが含まれていた場合はカンマ区切りで返す(){
    +
    +    function testTweetをパースするhashtagとreplyが含まれていた場合はカンマ区切りで返す() {
             $this->assertIdentical("!HashTag,Reply\t@Bob こんにちは #tddpc", $this->Mzp->categorize("AAA\t@Bob こんにちは #tddpc"));
         }
    +
    +    function testGetDataでデータをとってくる() {
    +        $results = $this->Mzp->getData();
    +        $this->assertTrue(is_array($results));
    +        #$this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
    +        $this->assertPattern('!.+,.+,.*!', $results[0]);
    +    }
    +
     }
    \ No newline at end of file

--- tests/cases/libs/mzp.test.php
+++ tests/cases/libs/mzp.test.php
@@ -13,22 +13,35 @@ class MzpTestCase extends CakeTestCase {
         ClassRegistry::flush();
     }
 
-    function testTweetをパースする(){
+    function testTweetをパースする() {
         $this->assertIdentical("Norlal\tあいうえお", $this->Mzp->categorize("Alice\tあいうえお"));
     }
-    function testTweetをパースする2(){
+
+    function testTweetをパースする2() {
         $this->assertIdentical("Norlal\tかきくけこ", $this->Mzp->categorize("Bob\tかきくけこ"));
     }
-    function testTweetをパースするrelpyを渡すとReplyをつけて返す(){
+
+    function testTweetをパースするrelpyを渡すとReplyをつけて返す() {
         $this->assertIdentical("Reply\t@Bob", $this->Mzp->categorize("AAA\t@Bob"));
     }
-    function testTweetをパースするmentionを渡すとMentionをつけて返す(){
+
+    function testTweetをパースするmentionを渡すとMentionをつけて返す() {
         $this->assertIdentical("Mention\tあああ@Bob", $this->Mzp->categorize("AAA\tあああ@Bob"));
     }
-    function testTweetをパースするhashtagを渡すとHashTagをつけて返す(){
+
+    function testTweetをパースするhashtagを渡すとHashTagをつけて返す() {
         $this->assertIdentical("!HashTag\tあああ #tddpc", $this->Mzp->categorize("AAA\tあああ #tddpc"));
     }
-    function testTweetをパースするhashtagとreplyが含まれていた場合はカンマ区切りで返す(){
+
+    function testTweetをパースするhashtagとreplyが含まれていた場合はカンマ区切りで返す() {
         $this->assertIdentical("!HashTag,Reply\t@Bob こんにちは #tddpc", $this->Mzp->categorize("AAA\t@Bob こんにちは #tddpc"));
     }
+
+    function testGetDataでデータをとってくる() {
+        $results = $this->Mzp->getData();
+        $this->assertTrue(is_array($results));
+        #$this->assertPattern('![0-9]{4}/(?:0[0-9]|1[0-2])/(?:[0-2][0-9]|3[0-1]) (?:[0-1][0-9]|2[0-3]):(?:[0-5][0-9]):(?:[0-5][0-9]),[A-z_],.*!', $results[0]);
+        $this->assertPattern('!.+,.+,.*!', $results[0]);
+    }
+
 }

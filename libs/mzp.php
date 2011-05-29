ddfd8c4146f9ae21c02d44268d7c407ccc19a704
nojimage <nojimage@gmail.com>
2011-05-29 17:15:07 +0900

    [from now] 2011年 5月29日 日曜日 17時15分07秒 JST
    diff --git a/libs/mzp.php b/libs/mzp.php
    index 9b6e366..6d7ef7e 100644
    --- a/libs/mzp.php
    +++ b/libs/mzp.php
    @@ -4,7 +4,9 @@ class Mzp {
    
         public function categorize($tweet) {
             $tweet = explode("\t", $tweet);
    -        if (!preg_match('/@/', $tweet[1]) && !preg_match('/#/', $tweet[1])) {
    +        if (preg_match('/#/', $tweet[1])) {
    +            $tweet[0] = '!HashTag';
    +        } else if (!preg_match('/@/', $tweet[1])) {
                 $tweet[0] = 'Norlal';
             }
             return join("\t", $tweet);

--- libs/mzp.php
+++ libs/mzp.php
@@ -4,7 +4,9 @@ class Mzp {
 
     public function categorize($tweet) {
         $tweet = explode("\t", $tweet);
-        if (!preg_match('/@/', $tweet[1]) && !preg_match('/#/', $tweet[1])) {
+        if (preg_match('/#/', $tweet[1])) {
+            $tweet[0] = '!HashTag';
+        } else if (!preg_match('/@/', $tweet[1])) {
             $tweet[0] = 'Norlal';
         }
         return join("\t", $tweet);

32d2429305163d959d7d67abb92efea986da857d
nojimage <nojimage@gmail.com>
2011-05-29 16:49:18 +0900

    fixes #1 Mzp::categorizeメソッドの実装

---
+++ libs/mzp.php
@@ -0,0 +1,13 @@
+<?php
+
+class Mzp {
+
+    public function categorize($tweet) {
+        $tweet = explode("\t", $tweet);
+        if (!preg_match('/@/', $tweet[1]) && !preg_match('/#/', $tweet[1])) {
+            $tweet[0] = 'Norlal';
+        }
+        return join("\t", $tweet);
+    }
+
+}
--- tests/cases/libs/mzp.test.php
+++ tests/cases/libs/mzp.test.php
@@ -13,4 +13,19 @@ class MzpTestCase extends CakeTestCase {
         ClassRegistry::flush();
     }
 
+    function testTweetをパースする(){
+        $this->assertIdentical("Norlal\tあいうえお", $this->Mzp->categorize("Alice\tあいうえお"));
+    }
+    function testTweetをパースする2(){
+        $this->assertIdentical("Norlal\tかきくけこ", $this->Mzp->categorize("Bob\tかきくけこ"));
+    }
+    function testTweetをパースする3(){
+        $this->assertIdentical("AAA\t@Bob", $this->Mzp->categorize("AAA\t@Bob"));
+    }
+    function testTweetをパースするmentionを渡すとそのまま返す(){
+        $this->assertIdentical("AAA\tあああ@Bob", $this->Mzp->categorize("AAA\tあああ@Bob"));
+    }
+    function testTweetをパースするhashtagを渡すとそのまま返す(){
+        $this->assertIdentical("AAA\tあああ #tddpc", $this->Mzp->categorize("AAA\tあああ #tddpc"));
+    }
 }
